9f7106f95fd49c95c3169f36577bfab9ffae4da3
Anton Zobnin <zobninatn@gmail.com>
2017-05-29 20:19:21 +0300

    Fix module lack

--- snippets.py
+++ snippets.py
@@ -1,9 +1,10 @@
 from tweepy import TweepError
+import time
 
 def retry(method):
     try:
         return method()
     except TweepError as err:
         if err.message[0]['code'] == 88:
-            sleep(60)
+            time.sleep(60)
             return method()
